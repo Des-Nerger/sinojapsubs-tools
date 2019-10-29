8a114800afaabfc18453f696664b90ec2d779438
Des-Nerger <mixerator@rambler.ru>
2019-10-29 12:25:07 +1000

    sophisticate blank-cjchars, handle Bopomofo and punctuation

--- blank-cjchars.go
+++ blank-cjchars.go
@@ -30,10 +30,41 @@ func main() {
 		default:
 			panic(err)
 		}
-		if unicode.In(r, unicode.Hiragana, unicode.Katakana, unicode.Han, FullwidthAlphaNum) {
+		switch r {
+		//case 'ー', '～', '・', '･':
+		case '…':
+			bw.WriteString("...")
+		case '！':
+			bw.WriteByte('!')
+		case '？':
+			bw.WriteByte('?')
+		case '～':
+			bw.WriteByte('~')
+		case '・', '･':
 			bw.WriteString(blankSequence)
-			continue
+		default:
+			if unicode.In(r, unicode.Pi, unicode.Ps) {
+				bw.WriteByte('[')
+			} else if unicode.In(r, unicode.Pf, unicode.Pe) {
+				bw.WriteByte(']')
+			} else if !unicode.In(r,
+				unicode.Hiragana,
+				unicode.Katakana,
+				unicode.Han,
+				FullwidthAlphaNum,
+				/*
+				unicode.Latin,
+				unicode.Punct,
+				unicode.Symbol,
+				*/
+				unicode.Lm,
+				unicode.Bopomofo,
+				unicode.Sk,
+			) {
+				bw.WriteRune(r)
+			} else {
+				bw.WriteString(blankSequence)
+			}
 		}
-		bw.WriteRune(r)
 	}
 }
